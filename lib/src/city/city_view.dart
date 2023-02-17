010cd5681791503c73c067cfd634d5455b3e0200
Пользователь <aygistov.ilya@gmail.com>
2023-02-17 12:46:18 +0200

    City TextField change

--- lib/src/city/city_view.dart
+++ lib/src/city/city_view.dart
@@ -19,44 +19,51 @@ class CityView extends StatelessWidget {
         title: const Text('City'),
         leading: const SizedBox(height: 0, width: 0),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: context.read<CityCubit>().controller,
-                    autofocus: true,
-                    onChanged: (value) {
-                      context.read<CityCubit>().updateCity(value.toString());
-                      context.read<WeatherCubit>().clearError();
-                    },
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      textAlign: TextAlign.center,
+                      decoration: const InputDecoration(
+                        hintText: 'Enter a city name',
+                      ),
+                      controller: context.read<CityCubit>().controller,
+                      autofocus: true,
+                      onChanged: (value) {
+                        context.read<CityCubit>().updateCity(value.toString());
+                        context.read<WeatherCubit>().clearError();
+                      },
+                    ),
                   ),
-                ),
-                IconButton(
-                    color: Theme.of(context).colorScheme.primary,
-                    onPressed: context.watch<CityCubit>().state.city.isEmpty
-                        ? null
-                        : () {
-                            context
-                                .read<WeatherCubit>()
-                                .getWeather(
-                                    context.read<CityCubit>().state.city)
-                                .then((ok) => {
-                                      if (ok)
-                                        {
-                                          Navigator.restorablePushNamed(
-                                              context, WeatherView.routeName)
-                                        }
-                                    });
-                          },
-                    icon: const Icon(Icons.search))
-              ],
-            ),
-            Text(context.watch<WeatherCubit>().state.errorText)
-          ],
+                  IconButton(
+                      color: Theme.of(context).colorScheme.primary,
+                      onPressed: context.watch<CityCubit>().state.city.isEmpty
+                          ? null
+                          : () {
+                              context
+                                  .read<WeatherCubit>()
+                                  .getWeather(
+                                      context.read<CityCubit>().state.city)
+                                  .then((ok) => {
+                                        if (ok)
+                                          {
+                                            Navigator.restorablePushNamed(
+                                                context, WeatherView.routeName)
+                                          }
+                                      });
+                            },
+                      icon: const Icon(Icons.search))
+                ],
+              ),
+              Text(context.watch<WeatherCubit>().state.errorText)
+            ],
+          ),
         ),
       ),
     );
